35dc9813a0acc492106d69348f24b7f7261d59a5
Javi Almirante <almirante.javi@gmail.com>
2016-04-15 01:56:39 +0800

    no message

---
+++ 05-Implementations/Furrever Pawsitive/Assets/CatUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatUI : MonoBehaviour 
+{
+	public CatStats stats;
+	public Transform hungerLevel;
+	
+	void Start ()
+	{
+		stats = GetComponent<CatStats>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		hungerLevel.localScale = new Vector3(stats.GetHunger() / 100.00f, 1);
+		hungerLevel.localPosition = new Vector3((stats.GetHunger() / 100.00f - 1) * 0.5f, 0);
+
+	}
+}
